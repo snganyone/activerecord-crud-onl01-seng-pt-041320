09595a38a8d731bc1711ad9a959f34a5c65da7df
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-06-24 16:25:44 -0700

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -86,8 +86,11 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  movie = Movie.create(title: "Wat?")
-  movie.update(title: "Wat, huh")
+  Movie.create(title: "Wat?")
+  #Find record
+  movie = Movie.find_by(title: "Wat?")
+  #Update
+  movie.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
