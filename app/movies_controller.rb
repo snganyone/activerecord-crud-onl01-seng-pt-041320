d7e7428047e001dafc8215353d3b1029dcca9df7
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-06-23 20:57:14 -0700

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -37,7 +37,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  movie = Movie.new
+  binding.pry
 end
 
 def can_get_the_last_item_in_the_database
