edc1499fb70e5756bef17782ff2b0d5d496efc1f
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-06-23 23:01:33 -0700

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -69,7 +69,8 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  Movie.where(release_date > 2002)
+  sort = Movie.where("release_date > 2002").order(release_date: :desc)
+  sort
 end
 
 def can_be_found_updated_and_saved
