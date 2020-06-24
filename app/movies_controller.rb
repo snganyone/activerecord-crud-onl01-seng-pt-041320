5e69848078dcc905177fd8d35650291fde2851f4
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-06-23 20:34:58 -0700

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,7 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = hash)
+def can_be_created_in_a_block(title = "Home Alone", args = {})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
